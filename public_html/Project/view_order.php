f7ced9df711e6ce1ec1362c1ad61810fe518a8e2
Matea Milloshi <85270730+mateamilloshi@users.noreply.github.com>
2023-05-04 17:49:49 -0400

    alignment changes

--- public_html/Project/view_order.php
+++ public_html/Project/view_order.php
@@ -37,7 +37,7 @@ $results = [];
             <?php foreach ($results as $r): ?>
                 <?php $subtotal += ($r["unit_price"] * $r["quantity"]); ?>
 
-                <div class="card" style="width: 25rem; height: 25rem;">
+                <div class="card" style="width: 25rem; height: 25rem;margin: 0 auto;">
                 <div class="card-body">
                 <h5 class="card-title">Item: <?php echo($r["name"]); ?></h5>
                 <div>Price: <?php echo($r["unit_price"]); ?></div>
@@ -73,8 +73,8 @@ $res = $stmt->execute([":orderID" => $orderID]);
 
 <div class="results" style="margin-left: 50px;">
     <?php if (!empty($result)): ?>
-        <h5 style="text-align:left ; margin-left: 10px ;">Shipping Information</h5>
-                <div class="card" style="width: 15rem; height: 15rem;">
+        <h5 style="text-align:center; margin-left: 10px ;">Shipping Information</h5>
+                <div class="card" style="width: 15rem; height: 15rem;margin: 0 auto;">
                 <div class="card-body">
             <div><b>First Name: </b><?php echo($result["first_name"]); ?></div>
             <div><b>Last Name: </b><?php echo($result["last_name"]); ?></div>
